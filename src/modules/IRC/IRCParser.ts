aa3d0088533c1fdf44bfad2ca6419f70450dd273
Mike Pham <support@nativecode.com>
2017-10-29 18:07:02 -0400

    fixing parsing again...

--- src/modules/IRC/IRCParser.ts
+++ src/modules/IRC/IRCParser.ts
@@ -26,18 +26,22 @@ export class IRCParser {
     return record as IRCParserRecord
   }
 
-  private secret(property: string, value: string, secrets: IRCParserSecrets): string {
-    let secret = secrets[property]
-    if (secret && secret.toLowerCase().startsWith('env.')) {
-      const key = secret.replace('env.', '').toUpperCase()
-      secret = process.env[key] || secret
-      this.log.trace('replace from env', value, secret)
-      return secret
-    }
-    const regex = new RegExp(`{${property}}`, 'gm')
-    secret = value.replace(regex, secret)
-    this.log.trace('replace from regex', value, secret)
-    return secret
+  private secret(value: string, secrets: IRCParserSecrets): string {
+    let result = value
+    Object.keys(secrets)
+      .map((name: string) => ({ name, value: secrets[name] }))
+      .map((secret): string => {
+        const regex = new RegExp(`\{${secret.name}\}`, 'gm')
+        if (secret.value.toLowerCase().startsWith('env:')) {
+          const env = secret.value.replace('env:', '').toUpperCase()
+          this.log.trace('secret.regex', secret.name, value, result, env)
+          return result.replace(regex, process.env[env] || result)
+        }
+        return result.replace(regex, result)
+      })
+
+    this.log.trace('secret', value, result)
+    return result
   }
 
   private transform(value: string, index: number, record: IRCParserRecordMap): IRCParserRecordMap {
@@ -46,12 +50,13 @@ export class IRCParser {
     if (formatter && formatter.regex) {
       const regex = new RegExp(formatter.regex)
       if (regex.test(value)) {
-        const formatted = record[property] = this.secret(property, formatter.replace, this.options.secrets)
-        this.log.trace('formatted', value, formatted)
+        const formatted = record[property] = this.secret(formatter.replace, this.options.secrets)
+        this.log.trace('regex', value, formatted)
       }
       this.log.traceJSON(record)
     } else {
-      record[property] = this.secret(property, value, this.options.secrets)
+      const formatted = record[property] = this.secret(value, this.options.secrets)
+      this.log.trace('replace', value, formatted)
     }
     return record
   }
