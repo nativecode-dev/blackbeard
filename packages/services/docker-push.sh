8546ba42ed0585eb40fa15b5139a95b39cd2b50f
Mike Pham <support@nativecode.com>
2017-11-12 02:05:33 -0500

    adding help with debugging travis

--- packages/services/docker-push.sh
+++ packages/services/docker-push.sh
@@ -4,22 +4,28 @@ set -e
 
 source .env.sh
 
-if [ $BRANCH = "master" ]; then
+if [ $BRANCH = "master" or $TRAVIS_BRANCH = "master" ]; then
 
   # Create docker images first.
   yarn run docker
 
+  # LATEST
   $DOCKER tag $DOCKER_TAG $DOCKER_REPO:latest
   $DOCKER image push $DOCKER_TAG
   $DOCKER image push $DOCKER_REPO:latest
 
+  # IRCWATCH
   $DOCKER tag $DOCKER_REPO-ircwatch:$DOCKER_VERSION $DOCKER_REPO-ircwatch:latest
   $DOCKER image push $DOCKER_REPO-ircwatch:$DOCKER_VERSION
   $DOCKER image push $DOCKER_REPO-ircwatch:latest
 
+  # SCHEDULER
   $DOCKER tag $DOCKER_REPO-scheduler:$DOCKER_VERSION $DOCKER_REPO-scheduler:latest
   $DOCKER image push $DOCKER_REPO-scheduler:$DOCKER_VERSION
   $DOCKER image push $DOCKER_REPO-scheduler:latest
+
 else
-  echo "Current branch is not master. Refusing to push docker images."
+
+  echo "Current branch ($BRANCH) is not master. Refusing to push docker images."
+
 fi
